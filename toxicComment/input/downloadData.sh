36226afa64b575ddbd0a3494735b9533aba6d173
Vaibhav Singh <singh.vaibhav.83@gmail.com>
2018-02-04 11:34:09 -0800

    Initial draft for Toxic Comment Classification Project

---
+++ toxicComment/input/downloadData.sh
@@ -0,0 +1,3 @@
+wget https://www.kaggle.com/c/jigsaw-toxic-comment-classification-challenge/download/sample_submission.csv.zip
+wget https://www.kaggle.com/c/jigsaw-toxic-comment-classification-challenge/download/test.csv.zip
+wget https://www.kaggle.com/c/jigsaw-toxic-comment-classification-challenge/download/train.csv.zip
